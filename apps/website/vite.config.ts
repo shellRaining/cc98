7913788c8a8274b10c09ed9b53dd6643fced8ced
shellRaining <shellRaining@gmail.com>
2026-07-22 19:02:27 +0800

    fix(build): 精简 Vercel 构建日志

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
+  logLevel: "warn",
   plugins: [UnoCSS(), vue()],
   build: {
     reportCompressedSize: false,
